09c0f7b1f3fe176be1eaee7b54b8c92e0fce6042
bryam <layhamshavii@gmail.com>
2025-04-22 15:19:40 +0000

    semaforo bryam

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,11 +1,29 @@
-import React from "react";
-import TrafficLight from "./TrafficLight";
+import React, { useState } from "react";
 
 const Home = () => {
+  const[colors, setColors] = useState("red")
+
+  const randonLidth =() => {
+    const colores = [ "redLigth", "greenLigth", "yellowLigth",]
+    const random = colores [Math.floor (Math.random()* colores.length)]
+    setColors(random)
+  }
+  
+  const handleClick = (val) => {
+    setColors (val)
+  }
+
   return (
-    <div className=" Container d-flex justify-content-center align-items-center flex-colunm vh-100">
-      <TrafficLight /> 
+    <div className="conteiner-fluid">
+      <div className="Semaforo"></div>
+        <div className="semaforoContainer">
+          <div className={`redLigth ${colors === "redLigth" ? "iluminar" : " "}`} onClick={()=>handleClick('redLigth')}></div>
+          <div className={`yellowLigth ${colors === "yellowLigth" ? "iluminar" : " "}`} onClick={()=>handleClick('yellowLigth')}></div>
+          <div className={`greenLigth ${colors === "greenLigth" ? "iluminar" : " "}`} onClick={()=>handleClick('greenLigth')}></div> </div>
+    
+    
     </div>
+    
   );
 };
 
